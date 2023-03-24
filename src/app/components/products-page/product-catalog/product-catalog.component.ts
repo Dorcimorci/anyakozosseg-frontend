729d95a57759f3258ff7faef80fd670954040951
DorotyIT <dorottya1203@gmail.com>
2023-03-24 10:27:59 +0100

    adjust product-list and -catalog

--- src/app/components/products-page/product-catalog/product-catalog.component.ts
+++ src/app/components/products-page/product-catalog/product-catalog.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { PageAction } from '../../shared/enums';
+import { alphabetLetters } from '../../shared/utils';
 import { ProductListItem } from '../product-model/product.api';
 import { ProductService } from '../product-service/product.service';
 
@@ -8,27 +11,27 @@ import { ProductService } from '../product-service/product.service';
   templateUrl: './product-catalog.component.html',
   styleUrls: ['./product-catalog.component.scss'],
 })
-export class ProductCatalogComponent implements OnInit {
-  public products: ProductListItem[] = [];
+export class ProductCatalogComponent {
+  public products$: Observable<ProductListItem[]>;
+  public alphabetLetters: string[] = alphabetLetters;
   public activeLetter: string = 'a';
+
+  public pageAction: PageAction = PageAction.Read;
+  public get PageAction() {
+    return PageAction;
+  }
 
   constructor(
     private readonly productService: ProductService,
     private readonly activatedRoute: ActivatedRoute
   ) {
-    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
-      const abcLetter: string | null = paramMap.get('abcLetter');
-      if (abcLetter) {
-        this.activeLetter = abcLetter;
-      }
-    });
-  }
-
-  public ngOnInit(): void {
-    this.productService
-      .fetchProductsByFirstLetter(this.activeLetter)
-      .subscribe((products: ProductListItem[]) => {
-        this.products = products;
-      });
+    this.products$ = this.activatedRoute.paramMap.pipe(
+      switchMap((paramMap: ParamMap) => {
+        this.activeLetter = paramMap.get('abcLetter') ?? this.activeLetter;
+        return this.productService.fetchProductsByFirstLetter(
+          this.activeLetter
+        );
+      })
+    );
   }
 }
--- src/app/components/products-page/product-list/product-list.component.ts
+++ src/app/components/products-page/product-list/product-list.component.ts
@@ -1,11 +1,59 @@
-import { Component, Input } from '@angular/core';
-import { ProductListItem } from '../product-model/product.api';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { take } from 'rxjs';
+import { ProductListItem, Rating } from '../product-model/product.api';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
+  @ViewChildren('comment') commentElements!: QueryList<ElementRef>;
+
   @Input() products: ProductListItem[] = [];
+
+  public showFullCommentBtnHovered: boolean = false;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  public ngAfterViewInit(): void {
+    this.commentElements.changes.subscribe(() => {
+      this.commentElements.forEach((comment: ElementRef, i: number) => {
+        const commentText = comment.nativeElement.innerText;
+        const isOverflowing =
+          comment.nativeElement.scrollHeight >
+          comment.nativeElement.clientHeight;
+        this.products[i].lastRating.isEllipsisActive = isOverflowing;
+        this.cd.detectChanges();
+      });
+    });
+  }
+
+  public isCommentOverflowing(comment: ElementRef): boolean {
+    return (
+      comment.nativeElement.scrollWidth > comment.nativeElement.clientWidth
+    );
+  }
+
+  public onCommentClick(lastRating: Rating): void {
+    lastRating.showFully = !lastRating.showFully;
+  }
+  public onMouseOver(): void {
+    this.showFullCommentBtnHovered = true;
+    console.log(this.showFullCommentBtnHovered);
+  }
+
+  public onMouseLeave(): void {
+    this.showFullCommentBtnHovered = false;
+    console.log(this.showFullCommentBtnHovered);
+  }
 }
